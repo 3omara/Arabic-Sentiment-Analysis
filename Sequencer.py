7405ce53ae758bc21033276bdcb0822d21881fb9
3omara <youssef.ahmed.ibrahim@outlook.com>
2022-09-27 16:19:25 +0200

    CBOW trial + final touches

--- Sequencer.py
+++ Sequencer.py
@@ -8,26 +8,7 @@ class Sequencer():
 
         self.tweets = tweets
         self.embed = embedding_dict
-        #self.min_count = min_count
         self.padding = padding
-        
-        # self.corpus = (" ".join(self.tweets.to_numpy().flatten())).split()
-
-        # freq = {}
-
-        # for i, word in enumerate(self.corpus):
-        #     try:
-        #         freq[word] += 1
-        #     except:
-        #         freq[word] = 1
-
-        # self.sorted_freq = {key: val for key, val in sorted(freq.items(), key = lambda ele: ele[1], reverse=1)}
-        # embedding_vocab = [word for word, val in self.sorted_freq.items() if val >= self.min_count]
-
-        # self.word2embedding = {}
-        # for i, word in enumerate(embedding_vocab):
-        #     self.word2embedding[word] = embedding_matrix[i]
-
 
     def text_to_vec(self, tweet):
         vec = []
